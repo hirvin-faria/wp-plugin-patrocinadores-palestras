ce8ab3024b4c9a3c9d5d81f13cb66850fc60d982
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-09-01 08:39:18 -0300

    :tada: Iniciando Plugin
    Iniciando plugin Alura Patrocinadores

---
+++ al-patrocinadores-palestras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Patrocinadores Alura
+ * Description: Selecionar patrocinadores da palestra.
+ * Version: 1.0.0
+ * Author: Hirvin Faria
+ */
+
+
+/**
+ * Verificação para que o plugin seja chamado do mesmo diretório onde o wp esteja instalado
+ */
+if(!defined('ABSPATH')) {
+    die;
+}
